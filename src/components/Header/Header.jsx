70311f7caabccc59349ed68ed7ef90842b166ca6
FM-Akib <muntasirakib08@gmail.com>
2024-01-31 17:16:39 +0600

    nav added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
 import logo from '../../assets/logoD.png';
+import './Header.css';
+import { TERipple } from 'tw-elements-react';
+import { Link } from 'react-router-dom';
+
+
+
 const Header = () => {
     return (
        <div>
             <div className="upHeader">
-                    <h3>Hotline: (+00) 545 545 323</h3>
+                    <h3>Hotline: (+880) 170 174 1656</h3>
                     <img src={logo} alt="logo"/>
+
+                    <div className="">
+
+                    <TERipple rippleColor="light">
+                    <button type="button"
+                    className="mr-4 tracking-wider inline-block rounded bg-success px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-success-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-success-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-success-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+                    Login
+                    </button>
+                    </TERipple>
+
+                    <TERipple rippleColor="light">
+                    <button type="button"
+                    className="tracking-wider inline-block rounded bg-danger px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-danger-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-danger-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-danger-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+                    Sign Up
+                    </button>
+                    </TERipple>
+
+
+                    </div>
             </div>
+        <nav className='NavBar'>
+            <ul className="navItems">
+                <Link to="/">Home</Link>
+                <Link to="/">About us</Link>
+                <Link to="/">Donation</Link>
+                <Link to="/">Blood Bank</Link>
+            </ul>
+        </nav>
+          
        </div>
     );
 };

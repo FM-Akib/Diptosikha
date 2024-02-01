4834bf5925a7998162bdbe909d0b471d85b77f05
FM-Akib <muntasirakib08@gmail.com>
2024-02-01 17:09:27 +0600

    updated

--- src/components/Teamperson/Teamperson.jsx
+++ src/components/Teamperson/Teamperson.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Teamperson = ({person}) => {
-    const {name}=person;
+    const {name,designation,img}=person;
     return (
        
 
@@ -11,11 +11,11 @@ const Teamperson = ({person}) => {
                 <img className="object-cover object-top w-full" src='https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='Mountain'/>
             </div>
             <div className="mx-auto w-32 h-32 relative -mt-16 border-4 border-white rounded-full overflow-hidden">
-                <img className="object-cover object-center h-32" src='https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ' alt='Woman looking front'/>
+                <img className="object-cover object-center h-32" src={img} alt='Woman looking front'/>
             </div>
             <div className="text-center mt-2">
-                <h2 className="font-semibold">Sarah Smith</h2>
-                <p className="text-gray-500">Freelance Web Designer</p>
+                <h2 className="font-semibold">{name}</h2>
+                <p className="text-gray-500">{designation}</p>
             </div>
             <ul className="py-4 mt-2 text-gray-700 flex items-center justify-around">
                 <li className="flex flex-col items-center justify-around">

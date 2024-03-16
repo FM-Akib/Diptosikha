3c074af65894c0b215857c46b0d58ff90a89aea3
FM-Akib <muntasirakib08@gmail.com>
2024-03-16 09:34:24 +0600

    dashboard added

---
+++ src/components/Profile/User.jsx
@@ -0,0 +1,10 @@
+
+const User = () => {
+    return (
+        <div className="flex justify-center items-center">
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint animi culpa pariatur perferendis necessitatibus ullam consectetur, iure non odit similique delectus velit dolores repudiandae fuga consequatur corporis voluptas ex doloribus maiores, laborum optio magnam. Doloribus non quae, libero officia inventore, vitae sequi praesentium sint excepturi dolorum, numquam quasi sapiente nemo quo? Magni tempora quasi ullam dicta, architecto expedita labore nam vero ea aut atque qui illum suscipit, vel, aliquid fugit deserunt repellat rerum nulla in earum iusto explicabo? Vel quos perferendis maxime magni magnam expedita explicabo tempora totam reiciendis similique? Eveniet quam saepe iste dolorum id itaque architecto. Aperiam, qui tempore atque accusantium temporibus voluptatem alias. Consectetur, officiis reiciendis deleniti possimus corrupti minus totam nam debitis praesentium architecto eius doloremque. Modi dolores at laudantium, quam recusandae in corporis repellat facere doloremque pariatur, eos veniam nemo voluptate maxime reprehenderit ipsum? Nihil dolore, natus eos vel dolorum saepe expedita ex in adipisci voluptatibus maxime ipsa quos cumque tempore qui. Veritatis, totam. Possimus, adipisci quidem. Repellat quas delectus veritatis quam optio ut consequatur molestias. Eum fugit dolore quam vero laboriosam, accusamus quasi temporibus, aperiam non est amet! Voluptate consectetur animi blanditiis nulla hic libero possimus voluptatum nisi officiis, quo molestiae iste minima. Quo!</h1>
+        </div>
+    );
+};
+
+export default User;
